c293dec6b7c456a9d20db1f18e40a3ae52a58e12
Piyushsharma218 <sharmasatyam524@gmail.com>
2025-12-17 01:28:12 +0530

    created navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { House } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <header className="sticky top-0 bg-gray-950/95 backdrop-blur-md text-white shadow-2xl  border-b border-orange-900">
+        <div className="container mx-auto p-4 flex justify-between items-center">
+          <Link to={"/"}>
+            <div className="flex items-center space-x-3 crusor-pointer">
+              <House className="w-8 h-8 text-orange-400 " />
+              <h1 className="text-4xl font-extrabold tracking-widest">
+                WDM<span className="text-orange-400">STORE </span>
+              </h1>
+            </div>
+          </Link>
+
+          <nav className="flex items-center space-x-6">
+            <Link
+              to={"/cart"}
+              className=" relative p-3 bg-orange-500/10 rounded-xl text-orange-400 hover:bg-orange-500/20 transit:duration-200 border border-orange-400/50shadow-lg cursor-pointer"
+            >
+              <ShoppingCart />
+            </Link>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
