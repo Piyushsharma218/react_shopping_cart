3f34edfa87809b2d508a363457c8ca1f25209348
Piyushsharma218 <sharmasatyam524@gmail.com>
2025-12-18 00:52:53 +0530

    created search bar and category filter

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { initialProducts } from "../data/product";
+
+import { Tag } from "lucide-react";
+
+const availablecategories = [
+  "all",
+  ...new Set(initialProducts.map((p) => p.category)),
+];
 
 const CategoryFilter = () => {
+  const selected = "Phone";
   return (
-    <div>CategoryFilter</div>
-  )
-}
+    <>
+      <div className="flex gap-3 border-b border-gray-800 pb-6 ">
+        <Tag className="w-5 h-5 text-orange-500 mt-2 mr-2 " />
 
-export default CategoryFilter
+        {availablecategories.map((category) => (
+          <button
+            key={category}
+            className={`px-5 py-2 text-sm font-bold rounded-full  ${
+              selected === category
+                ? "bg-orange-600 text-white "
+                : "bg-gray-800 text-gray-300 hover:bg-gray-700"
+            }`}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default CategoryFilter;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import SearchFilter from '../components/SearchFilter'
+import { Search } from 'lucide-react'
+import CategoryFilter from '../components/CategoryFilter'
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <>
+    <div className='mx-auto px-4 md:px-8 pt-8'>
+      
+    <SearchFilter />
+    <CategoryFilter />
+    </div>
+    </>
   )
 }
 
